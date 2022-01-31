61ee63cb6a40ff7fe2cb06dfa6f42080dc838337
ITHwang <intaek428@gmail.com>
2022-01-31 23:20:47 +0900

    learned effects of react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import Button from './Button';
 import styles from './App.module.css';
+import { useEffect, useState } from 'react';
+
+function Hello() {
+	useEffect(() => {
+		console.log('hi:)');
+		return () => console.log('bye:(');
+	}, []);
+	return <h1>Hello</h1>;
+}
 
 function App() {
-  return (
-    <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={'Continue'}/>
-    </div>
-  );
+	const [showing, setShowing] = useState(false);
+	const onClick = () => setShowing((prev) => !prev);
+	return (
+		<div>
+			{showing ? <Hello /> : null}
+			<button onClick={onClick}>{showing ? 'hide' : 'show'}</button>
+		</div>
+	);
 }
 
 export default App;
