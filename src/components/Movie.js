408175ffec393e25e9f0b4c0e39cd716ac850379
ITHwang <intaek428@gmail.com>
2022-02-02 00:56:59 +0900

    done

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,13 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-function Movie({ mediumCoverImage, title, summary, genres }) {
+function Movie({ id, mediumCoverImage, title, summary, genres }) {
 	return (
 		<div>
 			<img src={mediumCoverImage} alt={title} />
-			<h1>{title}</h1>
+			<h1>
+				<Link to={`/movie/${id}`}>{title}</Link>
+			</h1>
 			<p>{summary}</p>
 			<ul>
 				{genres.map((g) => (
@@ -16,6 +19,7 @@ function Movie({ mediumCoverImage, title, summary, genres }) {
 }
 
 Movie.propTypes = {
+	id: PropTypes.number.isRequired,
 	mediumCoverImage: PropTypes.string.isRequired,
 	title: PropTypes.string.isRequired,
 	summary: PropTypes.string.isRequired,
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,15 @@
+import { useEffect, useState } from 'react';
+import Movie from '../components/Movie';
+
 function Home() {
 	const [loading, setLoading] = useState(true);
 	const [movies, setMovies] = useState([]);
+
 	const getMovies = async () => {
 		const response = await fetch(
 			`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
 		);
+
 		const json = await response.json();
 		setMovies(json.data.movies);
 		setLoading(false);
@@ -20,6 +25,7 @@ function Home() {
 					{movies.map((movie) => (
 						<Movie
 							key={movie.id}
+							id={movie.id}
 							mediumCoverImage={movie.medium_cover_image}
 							title={movie.title}
 							summary={movie.summary}
