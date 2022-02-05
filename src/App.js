45c290b1e45e5d68f66606281231698dc597808a
ITHwang <intaek428@gmail.com>
2022-02-05 17:49:50 +0900

    add useTitle hook into App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,36 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import Detail from './routes/Detail';
 
+import { useState, useEffect } from 'react';
+
+const useTitle = (initTitle) => {
+	const [title, setTitle] = useState(initTitle);
+
+	const updateTitle = () => {
+		const htmlTitle = document.querySelector('title');
+		htmlTitle.innerHTML = title;
+	};
+
+	useEffect(updateTitle, [title]);
+
+	return setTitle;
+};
+
 const App = () => {
+	const titleUpdater = useTitle('Loading...');
+
+	setTimeout(() => titleUpdater('home'), 3000);
+
 	return (
-		<Router>
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/movie/:id' element={<Detail />} />
-			</Routes>
-		</Router>
+		// <Router>
+		// 	<Routes>
+		// 		<Route path='/' element={<Home />} />
+		// 		<Route path='/movie/:id' element={<Detail />} />
+		// 	</Routes>
+		// </Router>
+		<div className='App'>
+			<div>Hi</div>
+		</div>
 	);
 };
 
