ec012370c9a8f7c29c058a86eecba5bafa7fdb59
ITHwang <intaek428@gmail.com>
2022-02-13 00:46:04 +0900

    add useScroll and useFullscreen into app.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Detail from './routes/Detail';
 import { Offline } from './routes/Offline';
 import { useNetwork } from './hooks/useNetwork';
 
+import { useScroll } from './hooks/useScroll';
+import { useFullscreen } from './hooks/useFullscreen';
+
 const App = () => {
 	const handleNetworkChange = (isOnline) => {
 		if (!isOnline) {
@@ -13,15 +16,33 @@ const App = () => {
 	};
 	const isOnline = useNetwork(handleNetworkChange);
 
+	const { y } = useScroll();
+
+	const onFullS = (isFull) => {
+		console.log(isFull ? 'We are full' : 'We are small');
+	};
+	const { element, triggerFull, exitFull } = useFullscreen(onFullS);
+
 	return (
-		// <div className='App'></div>
-		<Router>
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/movie/:id' element={<Detail />} />
-				<Route path='/offline' element={<Offline />} />
-			</Routes>
-		</Router>
+		<div className='App' style={{ height: '1000vh' }}>
+			<h1 style={{ position: 'fixed', color: y > 100 ? 'red' : 'blue' }}>Hi</h1>
+		</div>
+
+		// <div className='App' style={{ height: '1000vh' }}>
+		// 	<div ref={element}>
+		// 		<img src='https://picsum.photos/500/300' />
+		// 		<button onClick={exitFull}>Exit fullscreen</button>
+		// 	</div>
+		// 	<button onClick={triggerFull}>Make fullscreen</button>
+		// </div>
+
+		// <Router>
+		// 	<Routes>
+		// 		<Route path='/' element={<Home />} />
+		// 		<Route path='/movie/:id' element={<Detail />} />
+		// 		<Route path='/offline' element={<Offline />} />
+		// 	</Routes>
+		// </Router>
 	);
 };
 
